29475f37b8c443e21f93ef1cb6e0efb37f261589
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-08-15 14:13:07 +0900

    file1

---
+++ file1.py
@@ -0,0 +1,12 @@
+my_name = "nico"
+age = 12
+dead = False
+print("Hello my name is", my_name)
+print("and I'm", age, " years old")
+
+my_hobby = "Watch the movies"
+travel = 'JeJu'
+print("I recentry went to", travel)
+print("and I did there", my_hobby)
+
+def 
