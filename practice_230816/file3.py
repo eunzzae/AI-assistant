481822a437d2da01b5bca415c6e82fe06aced803
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-08-16 22:27:59 +0900

    file3

---
+++ practice_230816/file3.py
@@ -0,0 +1,21 @@
+"""distance = 0
+
+while distance <10:
+    print("I'm running:", distance, "km")
+    distance = distance +2"""
+    
+
+from random import randint
+
+print("Welcome to Python Casino")
+pc_choice = randint(1, 100)
+
+playint = True
+while playing:
+    if user_choice = int(input("Choose number (1-100):"))
+        print("You won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")
