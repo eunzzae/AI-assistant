803eec952cf461edb536624031021dc247b5d1d7
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-08-16 22:01:14 +0900

    file1

--- practice_230816/file1.py
+++ practice_230816/file1.py
@@ -1,2 +1,11 @@
-age = input("How old are you?")
-print("user answer", age)
+age = int(input("How old are you?"))
+
+if age < 18:
+    print("you can't drink.")
+elif age > 18 and age < 35:
+    print("you drink beer!")
+elif age == 60 or age ==70:
+    print("Birthday party!")
+else:
+    print("go ahead!")
+
