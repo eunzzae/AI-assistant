29d2775baa7314101dce35e8680f05ef8a8f9613
MY_dmsco <130825500+eunzzae@users.noreply.github.com>
2023-08-17 12:24:27 +0900

    file4

--- practice_230816/file4.py
+++ practice_230816/file4.py
@@ -1,5 +1,12 @@
-# days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+print(days_of_week)
+
+# days_of_week.append("Sat")
+# print(days_of_week)
+# days_of_week.append("Sun")
 # print(days_of_week)
 
-name = "nico"
-print(name.replace("o","🍵"))
+# days_of_week.remove("Fri")
+# print(days_of_week)
+
+print(days_of_week[2])
